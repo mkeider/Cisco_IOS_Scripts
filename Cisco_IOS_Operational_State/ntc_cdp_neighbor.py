73386f441ca754daa227f8e5df16b1e60b813c91
mkeider <mskeider@gmail.com>
2018-08-22 10:48:52 -0400

    Added logging feature

--- Cisco_IOS_Operational_State/ntc_cdp_neighbor.py
+++ Cisco_IOS_Operational_State/ntc_cdp_neighbor.py
@@ -43,6 +43,9 @@ with open('devices.json') as dev_file:
 
 cdp_results = []
 
+results = {'Successful':[], 'Failed':[]}
+
+
 
 for device in devices:
     device['username'] = username
@@ -60,11 +63,16 @@ for device in devices:
             cdp_results.append(item) #For Loop to append the OUTPUT results from connection.send_command
 
 
-            connection.disconnect()
+        connection.disconnect()
+        results['Successful'].append(device['ip'])
 
     except netmiko_exceptions as e:
         print('Failed to', device, e)
+        results['Failed'].append('.'.join((device['ip'], str(e))))
 
+print(json.dumps(results, indent=2))
+with open('results.json', 'w') as results_file:
+    json.dump(results, results_file, indent=2)
 
 with open('connections.csv','w', newline='')as new_file:
    fieldnames = ['local_host','local_interface','neighbor','neighbor_interface', 'platform', 'capability']
